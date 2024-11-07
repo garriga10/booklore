8761003247270318c30801f18caf5ae85b4bc2be
Victor Loiseau <vic.loiseau@gmail.com>
2024-11-07 10:40:16 +0100

    reorganized the folders and modified the output of reco api

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,8 @@ FROM python:3.10-slim
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 
-COPY package_folder package_folder
-COPY package_folder/models package_folder/models
+COPY package_folder/workflow package_folder/workflow
+COPY package_folder/workflow/models package_folder/workflow/models
 COPY raw_data raw_data
 
-CMD uvicorn package_folder.api_file:app --host 0.0.0.0 --port $PORT
+CMD uvicorn package_folder.workflow.api_file:app --host 0.0.0.0 --port $PORT
--- package_folder/workflow/model_reco_1.py
+++ package_folder/workflow/model_reco_1.py
@@ -1,16 +1,14 @@
 import os
 import pickle
-from package_folder.models.model_settings import model_settings
-from package_folder.models.data_load_and_process import load_and_preprocess
+from package_folder.workflow.data_load_and_process import load_and_preprocess
 
 ROOT_PATH = os.path.dirname(os.path.dirname(__file__))
 
-filtered_df = load_and_preprocess()[0]
-book_features = load_and_preprocess()[1]
+filtered_df,book_features = load_and_preprocess()
 
 
 def pred(bookid: str, top_n=5):
-    model_path = os.path.join(ROOT_PATH, 'models', 'model-reco-1.pkl')
+    model_path = os.path.join(ROOT_PATH, 'workflow','models', 'model-reco-1.pkl')
 
     with open(model_path, 'rb') as file:
         model = pickle.load(file)
@@ -18,12 +16,12 @@ def pred(bookid: str, top_n=5):
     # Extraire le numéro de l'identifiant du livre
     book_index = int(bookid.split('.')[0])
 
-    # Utiliser la ligne de DataFrame pour conserver les noms de colonnes
+    # Identifying the nearest neighbors of the bookid
     distances, indices = model.kneighbors(book_features.iloc[[book_index]], n_neighbors=top_n + 1)
 
-    recommended_books = filtered_df.iloc[indices[0][1:]]
+    recommended_books = filtered_df.iloc[indices[0][1:]] #filtering the original df with the nearest books we identified
     recommended_books = recommended_books[['title', 'author', 'publisher', 'rating']].copy()
     recommended_books['distance'] = distances[0][1:]
+    recommended_books = recommended_books.fillna("Not available")
     recommended_books = recommended_books[['title', 'author', 'publisher', 'rating', 'distance']].to_dict(orient='records')
-    #adding the Not Found string
     return recommended_books
